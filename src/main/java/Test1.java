61c22643f9dba9c5069183c78945898bd254ea52
zanel <2469214258@qq.com>
2023-02-14 16:19:16 +0800

    master second commit!

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
         System.out.println("hahaha");
         System.out.println("hahaha");
         System.out.println("hahaha");
-
+        System.out.println("master commit 1");
     }
 }
